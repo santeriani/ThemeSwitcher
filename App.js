ec54c319682b2f0619b5a29e6d553cd5427399a8
Santeri Anias <santeri.anias@gmail.com>
2024-02-26 15:42:29 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import MainAppbar from './components/MainAppbar';
+import Settings from './screens/Settings';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import { ThemeContext } from './context/ThemeContext';
+import ThemeProvider from './context/ThemeProvider';
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const Stack = createNativeStackNavigator()
 
+  return(
+    <PaperProvider>
+      <ThemeProvider>
+        <NavigationContainer>
+          <StatusBar backgroundColor="#666" barStyle="light-content"/>
+          <Stack.Navigator
+            initialRouteName='Home'
+            screenOptions={{header: (props) =>
+              <MainAppbar {...props} backgroundColor="#666" icon="cog" color="#fff" />
+            }}
+          >
+            <Stack.Screen name="Home">
+              {() =>
+                <Home />
+              }
+            </Stack.Screen>
+            <Stack.Screen name="Settings" component={Settings}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
+    </PaperProvider>
+  )
+}
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+*/
